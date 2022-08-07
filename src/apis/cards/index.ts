66749129f3cbac99d9c0f03f794ae067f8de55b9
Zack Anselm <rili.main@gmail.com>
2022-08-07 16:29:35 -0500

    feat: use card data to display player details

--- src/apis/cards/index.ts
+++ src/apis/cards/index.ts
@@ -4,13 +4,30 @@ export const QUERY_CARD = gql`
     query GetCard ($slug: String!) {
         card(slug: $slug) {
             age
+            cardEdition {
+                name
+            }
+            grade
+            name
             pictureUrl(derivative: "tinified")
             player {
+                country {
+                    slug
+                }
+                displayName
                 pictureUrl(derivative: "tinified")
             }
+            position
+            rarity
             season {
                 name
                 startYear
+            }
+            team {
+                ...on Club {
+                name
+                    pictureUrl(derivative: "tinified")
+                }
             }
         }
     }
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,15 +1,44 @@
 import classNames from 'classnames';
 import { useGetCardQuery } from '../generated/graphql';
 import placeholder from '../assets/img/card-placeholder.png';
+import cardBackgroundRed from '../assets/img/card-background-red.png';
+import cardBackgroundBlue from '../assets/img/card-background-blue.png';
+import cardFooterBackgroundRed from '../assets/img/card-footer-red.png';
+import cardFooterBackgroundBlue from '../assets/img/card-footer-blue.png';
+
+const redBackgroundId = 'marco-verratti-2021-unique-1';
+const blueBackgroundId = 'marco-verratti-2021-rare-1';
 
 interface CardProps {
     id: string;
+}
+
+const getBackground = (id: string) => {
+    if (id === redBackgroundId) {
+        return cardBackgroundRed;
+    }
+
+    return cardBackgroundBlue;
+}
+
+const getFooterBackground = (id: string) => {
+    if (id === redBackgroundId) {
+        return cardFooterBackgroundRed;
+    }
+
+    return cardFooterBackgroundBlue;
 }
 
 const Card = ({ id }: CardProps) => {
     const { data, error, loading } = useGetCardQuery({ variables: {
         slug: id
     } });
+
+    console.log(data?.card);
+
+    if (error) {
+        console.log(error);
+    }
 
     const cardImageClassName = classNames({
         'image-container': true,
@@ -21,13 +50,54 @@ const Card = ({ id }: CardProps) => {
         hidden: !loading,
     });
 
+    const footerGradientClassName = classNames({
+        'footer-gradient': true,
+        'alt-blue': id === blueBackgroundId,
+    })
+
     return (
         <div className="card-container">
             <div className={placeholderClassName}>
-                <img src={placeholder} />
+                <img src={placeholder} alt="player card placeholder"/>
             </div>
             <div className={cardImageClassName}>
-                <img src={data?.card?.pictureUrl || ''} />
+                <img src={getBackground(id)} alt="player card background"/>
+                <img src={data?.card?.player?.pictureUrl || ''} className="player-image" alt={data?.card?.player?.displayName} />
+                <img src={getFooterBackground(id)} className="footer-image"  alt="player card footer background"/>
+                {
+                    !loading &&
+                    <div className="card-header">
+                        <div className="header-left">
+                            <p className="season-text">{data?.card?.season.name}</p>
+                            <p className="edition-name">{data?.card?.cardEdition?.name}</p>
+                        </div>
+                        <div className="header-right">
+                            <p>
+                                <img src={(data?.card?.team as any || {}).pictureUrl || ''} className="team-image" alt={(data?.card?.team as any || {}).name} />
+                            </p>
+                            <p className="grade-text">{data?.card?.grade}</p>
+                        </div>
+                    </div>
+                }
+                <div className={footerGradientClassName}>
+                    <div className="player-name text-center">
+                        <p>{data?.card?.player?.displayName}</p>
+                    </div>
+                    <div className="player-details-container">
+                        <div className="details-group">
+                            <p className="details-header">Age</p>
+                            <p className="details">{data?.card?.age}</p>
+                        </div>
+                        <div className="details-group">
+                            <p className="details-header">Position</p>
+                            <p className="details">{data?.card?.position}</p>
+                        </div>
+                        <div className="details-group">
+                            <p className="details-header">Country</p>
+                            <p className="details">N/A</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
